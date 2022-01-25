0e8f41888c9763e8e5d5e765419927d6087ee678
Rohit Gulam <83160332+rohitgulam@users.noreply.github.com>
2022-01-25 19:20:32 +0300

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -15,7 +15,7 @@ $portNumber = 3306;
 
 $conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
 
-$pdo = new PDO('mysql:host=172.105.62.32;port=3306;dbname=qrmenu', $dBUserName, $dBPassword);
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=qrmenu', $dBUserName, $dBPassword);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if(!$conn){
