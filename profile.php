2e1b4a85a8cf01792b5c72f3f8db801ee9049457
Rohit Gulam <rohitsgulam@gmail.com>
2022-02-05 22:41:35 +0300

    Add styles to button and qr codes

--- profile.php
+++ profile.php
@@ -2,8 +2,11 @@
     include 'header.php'
 ?>
         <div class="qr-group">
-        <a href=<?php echo 'https://itsqrmenu.com/public/main.php?rest_id='.$_SESSION["userid"] ?> ><img src='<?php echo generateQR( 'https://itsqrmenu.com/public/main.php?rest_id='.$_SESSION["userid"]) ?>' alt='QR Code' width='200' height='200'></a>
-            <p>To print your QR codes, <br> screenshot this page and crop the above code then show it to the stationery for printing</p>
+            <div class="qr-group-inner">
+                <a href=<?php echo 'https://itsqrmenu.com/public/main.php?rest_id='.$_SESSION["userid"] ?> ><img src='<?php echo generateQR( 'https://itsqrmenu.com/public/main.php?rest_id='.$_SESSION["userid"]) ?>' alt='QR Code' width='220' height='220'></a>
+                <p>Scan this code with your phone's camera or Google lens</p>
+            </div>
+            <p>To print your QR codes, <br> screenshot this page and crop the above code then show it to a stationery for printing</p>
         </div>
 
         <div class="profile-container">
