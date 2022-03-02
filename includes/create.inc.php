407c77cbcf959b725ad862e0068553e56f63ad52
Rohit Gulam <rohitsgulam@gmail.com>
2022-03-02 12:28:47 +0300

    created select category input

--- includes/create.inc.php
+++ includes/create.inc.php
@@ -9,7 +9,10 @@ if (isset($_SESSION['useruid'])) {
         $food_name = $_GET['food_name']; 
         $food_price = $_GET['food_price']; 
         $food_desc = $_GET['food_desc']; 
-        $food_img = $_GET['food_img']; 
+        $food_category = $_GET['food_category']; 
+        // $food_img = $_GET['food_img']; 
+
+        
 
         require_once "dbh.inc.php";
         require_once "functions.inc.php";
@@ -19,7 +22,7 @@ if (isset($_SESSION['useruid'])) {
             exit();
         }
 
-        addFoodItem($conn, $userid, $food_name, $food_price, $food_desc);
+        addFoodItem($conn, $userid, $food_name, $food_price, $food_desc, $food_category);
 
     }
 }
