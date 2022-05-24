9e727db2766d24afc8847ce8b582d93d1875fe64
Rohit Gulam <rohitsgulam@gmail.com>
2022-05-24 18:07:07 +0300

    updated footer message

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer>
-        <p>If there is any problem, contact <a href="https://api.whatsapp.com/send/?phone=255679260086">Rohit</a></p>
+        <p>Contact <a href="https://api.whatsapp.com/send/?phone=255679260086">Rohit</a> for any help</p>
     </footer>
 </body>
 </html>
