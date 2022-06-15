7f61505d8563196ef8ee135cf24b3e043536e50d
Rohit Gulam <rohitsgulam@gmail.com>
2022-06-15 22:16:17 +0300

    Changed menu title to restaurant name

--- main.php
+++ main.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="assets/qr-code-1.svg" type="image/x-icon">
-    <title>Dashboard</title>
+    <title><?php echo $userDetails['rest_name'] ?> Menu</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;600&display=swap" rel="stylesheet">
